4a894ecec01fe206988e52504684de0619640d54
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-08-28 08:52:35 +0100

    findPostWithComments post from friends

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -3,8 +3,11 @@
 namespace App\Repository;
 
 use App\Entity\Post;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Post>
@@ -20,29 +23,87 @@ class PostRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Post::class);
     }
+    public function findAllWithComments(): array
+    {
+        return $this->createQueryBuilder('p') //alias to current entity
+            ->addSelect('c') //select all the fields for the comment entity realated to this post
+            ->leftJoin('p.comments', 'c') //c alias we give it to p.comments
+            ->orderBy('p.created', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+    public function findALlByAuthor(
+        int| User $author
+    ): array {
+        return $this->findAllQuerry(
+            withComments: true,
+            withLikes: true,
+            withAuthors: true
 
-//    /**
-//     * @return Post[] Returns an array of Post objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+        )->where('p.author= :author')
+            ->setParameter(
+                'author',
+                $author instanceof User ? $author->getId() : $author
+            )
+            ->getQuery()->getResult();
+    }
+    public function findAllQuerry(
+        bool $withComments = false,
+        bool $withLikes = false,
+        bool $withAuthors = false,
 
-//    public function findOneBySomeField($value): ?Post
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    ): QueryBuilder {
+        $query = $this->createQueryBuilder('p');
+        if ($withComments) {
+            $query->leftJoin('p.comments', 'c')
+                ->addSelect('c');
+        }
+        if ($withLikes) {
+            $query->leftJoin('p.likedBy', 'l')
+                ->addSelect('l');
+        }
+        if ($withAuthors) {
+            $query->leftJoin('p.author', 'a')
+                ->addSelect('a');
+        }
+        return $query->orderBy('p.created', 'DESC');
+    }
+    public function findPostsFromFollows(
+        Collection| array $authors
+    ): array {
+        return $this->findAllQuerry(
+            withComments: true,
+            withLikes: true,
+            withAuthors: true
+
+        )->where('p.author IN (:authors)')
+            ->setParameter(
+                'authors',
+                $authors
+            )->getQuery()->getResult();
+    }
+    //    /**
+    //     * @return Post[] Returns an array of Post objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Post
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
