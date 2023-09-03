0a69f787b627e4fff5b8e19985f3de6bf12ed93b
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-09-03 13:08:46 +0100

    post with pic new style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,14 +11,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function followsPosts(PostRepository $postRepository): Response
+    public function followsPosts(PostRepository $postRepository, UserRepository $userRepository): Response
     {
         /** @var User $currentUser */
         $currentUser = $this->getUser();
-        return $this->render('home/index.html.twig', [
-            'posts' => $postRepository->findPostsFromFollows(
-                $currentUser->getFollows()
-            ),
-        ]);
+        if ($currentUser->getFollows() !=null) 
+        {
+            return $this->render('home/index.html.twig', [
+                'posts' => $postRepository->findPostsFromFollows(
+                    $currentUser->getFollows()
+                ),
+            ]);
+        } else 
+        {
+            return $this->render('home/index.html.twig', [
+                'friends' => $userRepository->findAll(),
+
+            ]);
+        }
     }
 }
