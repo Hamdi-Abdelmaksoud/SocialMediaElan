afb17fbbab16e41dc410259b67346a6965b038e0
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-09-25 13:28:34 +0100

    friend list hide show

--- public/assets/js/asside.js
+++ public/assets/js/asside.js
@@ -1,30 +1,8 @@
-const menu = document.querySelector(".menu")
-const sideBar = document.querySelector(".sidebar_options")
-menu.addEventListener('click', () => {
-        sideBar.classList.toggle('menu_show')
-    })
-    // Fonction pour afficher la durée écoulée
-function afficherDureeEcoulee(dateString) {
-    const postDate = new Date(dateString);
-    const now = new Date();
-    const differenceEnMillisecondes = now - postDate;
-    const differenceEnSecondes = Math.floor(differenceEnMillisecondes / 1000);
-    const differenceEnMinutes = Math.floor(differenceEnSecondes / 60);
-    const differenceEnHeures = Math.floor(differenceEnMinutes / 60);
-    const differenceEnJours = Math.floor(differenceEnHeures / 24);
-
-    if (differenceEnSecondes < 60) {
-        return `il y a ${differenceEnSecondes} secondes`;
-    } else if (differenceEnMinutes < 60) {
-        return `il y a ${differenceEnMinutes} minutes`;
-    } else if (differenceEnHeures < 24) {
-        return `il y a ${differenceEnHeures} heures`;
+function toggle_div_fun(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "flex";
     } else {
-        return `il y a ${differenceEnJours} jours`;
+        element.style.display = "none";
     }
 }
-
-// Récupérez le contenu de la balise span et appelez la fonction
-const postTimeSpan = document.getElementById('postTime');
-const dateString = postTimeSpan.textContent.trim();
-postTimeSpan.textContent = afficherDureeEcoulee(dateString);
