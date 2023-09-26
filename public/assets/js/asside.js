9f077c43d8b1836667d3a3059ae592d812b9c44a
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-09-26 17:50:01 +0100

    add  comment Form

--- public/assets/js/asside.js
+++ public/assets/js/asside.js
@@ -1,4 +1,5 @@
 function toggle_div_fun(id) {
+
     var element = document.getElementById(id);
     if (element.style.display === "none" || element.style.display === "") {
         element.style.display = "flex";
